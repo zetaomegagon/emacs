4793171699b1e6744728f4c3ee35a3bbacb634e5
Elijah Beale <elijahthomasbeale+github@gmail.com>
2023-02-16 20:50:30 -0500

    build-emacs.sh: run as root and selectively build as normal user

--- build-emacs.sh
+++ build-emacs.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/env bash
 
-# update the system
-sudo dnf upgrade -y
-# install dependancies
-sudo dnf install -y libwebp-devel liblcms2-devel
-sudo dnf builddep -y emacs
-# link tree-sitter libs
-sudo ldconfig /usr/local/lib
+if [[ $USER == root ]]; then
+    # update the system
+    dnf upgrade -y
+    # install dependancies
+    dnf install -y libwebp-devel liblcms2-devel
+    dnf builddep -y emacs
+    # link tree-sitter libs
+    ldconfig /usr/local/lib
 
-# clean the repo and configure
-make extraclean \
-    && ./autogen.sh \
-    && ./configure --enable-check-lisp-object-type \
-		   --with-mailutils \
-		   --with-pgtk \
-		   --with-wide-int \
-		   --with-tree-sitter \
-		   --with-native-compilation=aot \
-		   PKG_CONFIG_PATH='/usr/local/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig'
-# begin build
-make -j $(nproc)
+    # clean the repo and configure
+    su ebeale -c make extraclean \
+	&& su ebeale -c ./autogen.sh \
+	&& su ebeale -c ./configure --enable-check-lisp-object-type \
+	      --with-mailutils \
+	      --with-pgtk \
+	      --with-wide-int \
+	      --with-tree-sitter \
+	      --with-native-compilation=aot \
+	      PKG_CONFIG_PATH='/usr/local/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig'
 
-# unistall existing emacs
-systemctl --user disable --now emacs.service
-sudo make uninstall
-# install new emacs
-sudo make install
-systemctl --user enable --now emacs.service
+    # begin build
+    su ebeale -c "make -j $(nproc)"
+
+    # unistall existing emacs
+    su ebeale -c 'systemctl --user disable --now emacs.service'
+    make uninstall
+    # install new emacs
+    make install
+    su ebeale -c 'systemctl --user enable --now emacs.service'
+else
+    echo "Run with sudo!"
+fi
