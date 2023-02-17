a0908b1c54736c6d1fdd902fdf1211976a05156c
Elijah Beale <elijahthomasbeale+github@gmail.com>
2023-02-17 00:19:18 -0500

    Changes to build-emacs.sh
    * bound regular username to $user variable
    * replaced hard coded instances of username with $user
    * added install gcc-c++ dependancy
    * now build / install tree-sitter from source
    * now build / install tree-sitter modules from source
    * remove unused eln files
    * fetch upstream and merge with local
    * removed systemctl calls because we can't connect to dbus intance
      as root

--- build-emacs.sh
+++ build-emacs.sh
@@ -1,34 +1,65 @@
 #!/usr/bin/env bash
 
+declare user; user=$USER
+
 if [[ $USER == root ]]; then
     # update the system
     dnf upgrade -y
+
     # install dependancies
-    dnf install -y libwebp-devel liblcms2-devel
+    dnf install -y libwebp-devel lcms2-devel gcc-c++
     dnf builddep -y emacs
+
+    # build and install current tree-sitter
+    (
+	cd ../tree-sitter/
+	make clean
+	git pull
+	su "$user" -c make
+	make install
+    )
+
+    # build and install tree-sitter-modules
+    (
+	cd ../tree-sitter-module/
+	rm -r dist/*
+	git pull
+	su "$user" -c ./batch.sh
+	chown -R root:root dist/*
+	mv dist/* /usr/local/lib/
+    )
+
     # link tree-sitter libs
     ldconfig /usr/local/lib
 
     # clean the repo and configure
-    su ebeale -c make extraclean \
-	&& su ebeale -c ./autogen.sh \
-	&& su ebeale -c ./configure --enable-check-lisp-object-type \
+    for dir in native-lisp/*; do
+	rm -r "$dir"
+	rm -r "/usr/local/lib/emacs/30.0.50/native-lisp/$dir"
+	rm -r "$HOME/.emacs.d/eln-cache/$dir"
+    done
+
+    make extraclean \
+	&& su "$user" 'git fetch upstream' \
+	&& su "$user" 'git merge --no-edit upstream/master' \
+	&& su "$user" 'git push -u origin master' \
+	&& su "$user" -c ./autogen.sh \
+	&& su "$user" -c "./configure --enable-check-lisp-object-type \
 	      --with-mailutils \
 	      --with-pgtk \
 	      --with-wide-int \
 	      --with-tree-sitter \
 	      --with-native-compilation=aot \
-	      PKG_CONFIG_PATH='/usr/local/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig'
+	      PKG_CONFIG_PATH='/usr/local/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig'"
 
     # begin build
-    su ebeale -c "make -j $(nproc)"
+    su "$user" -c "make -j $(nproc)"
 
     # unistall existing emacs
-    su ebeale -c 'systemctl --user disable --now emacs.service'
     make uninstall
+
     # install new emacs
     make install
-    su ebeale -c 'systemctl --user enable --now emacs.service'
 else
     echo "Run with sudo!"
 fi
